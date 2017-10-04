8ae6ff4e339d208e0a5637768670d87b9237388d
Mariyam Rajeev <g.mariyam@gmail.com>
2017-10-03 21:25:36 -0700

    file write code

--- src/main/java/kafka_consumer_ddos/kafka_consumer_ddos/App.java
+++ src/main/java/kafka_consumer_ddos/kafka_consumer_ddos/App.java
@@ -16,22 +16,24 @@ public class App
 	public static void main(String[] args) {
 	    Properties props = new Properties();
 	    props.put("bootstrap.servers", "localhost:9092");
-	    props.put("group.id", "abc1");
+	    props.put("group.id", "abc6");
 	    props.put("enable.auto.commit", "true");
 	    props.put("auto.commit.interval.ms", "1000");
 	    props.put("auto.offset.reset", "earliest");
 	    props.put("session.timeout.ms", "30000");
 	    props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 	    props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-
+	    DdosDetection ddosDetection = new DdosDetection();
 	    KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(props);
 	    kafkaConsumer.subscribe(Arrays.asList("ddos_ph"));
 	    while (true) {
 	    	try{
 	      ConsumerRecords<String, String> records = kafkaConsumer.poll(100);
 	      for (ConsumerRecord<String, String> record : records) {
-	        System.out.println("Partition: " + record.partition() + " Offset: " + record.offset()
-	            + " Value: " + record.value() + " ThreadID: " + Thread.currentThread().getId());
+	       /* System.out.println("Partition: " + record.partition() + " Offset: " + record.offset()
+	            + " Value: " + record.value() + " ThreadID: " + Thread.currentThread().getId());*/
+	    	  ddosDetection.findDdosDetection(record.value());
+	    	  System.out.println(record.value());
 	    	  
 	    	  
 	      }
--- src/main/java/kafka_consumer_ddos/kafka_consumer_ddos/DdosDetection.java
+++ src/main/java/kafka_consumer_ddos/kafka_consumer_ddos/DdosDetection.java
@@ -1,15 +1,106 @@
 package kafka_consumer_ddos.kafka_consumer_ddos;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class DdosDetection {
 	
-	private int threashold=5;
+	private int threashold=10;
+	private static final String ipv4Pattern = "(([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.){3}([01]?\\d\\d?|2[0-4]\\d|25[0-5])";
+	private static final String ipv6Pattern = "([0-9a-f]{1,4}:){7}([0-9a-f]){1,4}";
 	
-	public boolean findDdosDetection(String record)
+	
+	public void findDdosDetection(String record)
 	{
+		record = record.replace("(", "");
+		 record = record.replace(")", "");		
+		String [] recordArray = record.split(",");
+		System.out.println("record string"+recordArray[0]+" "+recordArray[1]);
+		// when the ip is found in the master list or the ip hit acount is greater than the threshold set
+		if(findIpAddressValid(recordArray))
+		{
 		
-		
-		return true;
+		if (validateIpwithMasterList(recordArray[0])|| Integer.parseInt(recordArray[1])>=threashold)		
+			writeResultToFile(recordArray);		
+		}
+		else
+			System.out.println("Not an attacker");
 		
 	}
+	
+	/*
+	 * validate the ip against a master list
+	 */
+	private boolean validateIpwithMasterList(String ip)
+	{
+		// for now creating a list, ideally read it from a database or a master list file. 
+		List<String> iplist = new ArrayList<String>();
+		iplist.add("129.192.143.166");
+		iplist.add("129.192.143.167");
+		iplist.add("129.192.143.168");
+		iplist.add("129.192.143.169");
+		
+		if (iplist.contains(ip))	
+			return true;
+		else
+			return false;
+	}
+	
+	/*
+	 * 
+	 * Write the results to a file
+	 */
+	
+	
+	public void writeResultToFile(String [] recordArray) {
+
+        PrintWriter outputStream = null;
+       try{
+           
+           
+           FileWriter fstream = new FileWriter("results.txt",true);
+           BufferedWriter fbw = new BufferedWriter(fstream);
+           fbw.write("IP:"+ recordArray[0]+" Count:"+recordArray[1]+"\n");
+           fbw.newLine();
+           fbw.close();
+       }
+       catch (IOException e){
+           System.out.println("Error opening the file result.txt.");
+           System.exit(0);
+       } 
+       
+
+    }
+	/*
+	 * i think this check is not needed, as it is safe to assume that apache log has ip address and therefor kafka queue
+	 */
+	private  boolean findIpAddressValid(String[] args)
+	{
+		String record ="(129.192.143.166,2)";
+		 
+		Pattern patternV4 = Pattern.compile(ipv4Pattern);
+		Pattern patternV6 = Pattern.compile(ipv6Pattern);
+		Matcher matcherV4 = patternV4.matcher(record);
+		Matcher matcherV6 = patternV4.matcher(record);
+		if (matcherV4.find() ) {
+		   return true;
+		} else if(matcherV6.find() ) {
+			return true;		
+		}
+		else
+		{
+			System.out.println( "0.0.0.0"); 
+			return false;
+			
+		}
+	}
+	
+	
 
 }
