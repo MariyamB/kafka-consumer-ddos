62142c61cb62d543e3c7c25f2503d4746758b869
Mariyam Rajeev <g.mariyam@gmail.com>
2017-10-03 19:48:26 -0700

    first commit

---
+++ src/main/java/kafka_consumer_ddos/kafka_consumer_ddos/App.java
@@ -0,0 +1,46 @@
+package kafka_consumer_ddos.kafka_consumer_ddos;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+/**
+ * Kafka consumer
+ *
+ */
+public class App 
+{
+	public static void main(String[] args) {
+	    Properties props = new Properties();
+	    props.put("bootstrap.servers", "localhost:9092");
+	    props.put("group.id", "abc1");
+	    props.put("enable.auto.commit", "true");
+	    props.put("auto.commit.interval.ms", "1000");
+	    props.put("auto.offset.reset", "earliest");
+	    props.put("session.timeout.ms", "30000");
+	    props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+	    props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
+	    KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(props);
+	    kafkaConsumer.subscribe(Arrays.asList("ddos_ph"));
+	    while (true) {
+	    	try{
+	      ConsumerRecords<String, String> records = kafkaConsumer.poll(100);
+	      for (ConsumerRecord<String, String> record : records) {
+	        System.out.println("Partition: " + record.partition() + " Offset: " + record.offset()
+	            + " Value: " + record.value() + " ThreadID: " + Thread.currentThread().getId());
+	    	  
+	    	  
+	      }
+	    	}
+	    	catch (Exception e)
+	    	{
+	    		System.out.println("Exception"+e.getMessage());
+	    	}
+	    }
+
+	  }
+}
---
+++ src/main/java/kafka_consumer_ddos/kafka_consumer_ddos/DdosDetection.java
@@ -0,0 +1,15 @@
+package kafka_consumer_ddos.kafka_consumer_ddos;
+
+public class DdosDetection {
+	
+	private int threashold=5;
+	
+	public boolean findDdosDetection(String record)
+	{
+		
+		
+		return true;
+		
+	}
+
+}
